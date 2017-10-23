0830c5a604b83b55fe659f35f5670f5451fb2fb7
PeterGIliev <piliev@gmail.com>
2017-10-23 20:02:14 +0300

    stage4 task4.1

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -1,4 +1,5 @@
 function startApp() {
+	if (sessionStorage.getItem('authToken') !== null) {        let username = sessionStorage.getItem('username');        $('#loggedInUser').text("Welcome, " + username + "!");    }
     showHideMenuLinks();
     showHomeView();
 
@@ -31,12 +32,14 @@ function startApp() {
             $("#linkRegister").show();
             $("#linkListAds").hide();
             $("#linkLogout").hide();
+            $("#loggedInUser").hide();
         } else {
             // We have logged in user
             $("#linkLogin").hide();
             $("#linkRegister").hide();
             $("#linkListAds").show();
             $("#linkLogout").show();
+            $("#loggedInUser").show();
         }
     }
 
@@ -85,6 +88,7 @@ function startApp() {
         sessionStorage.setItem('authToken', userAuth);
         let userId = userInfo._id;
         sessionStorage.setItem('userId', userId);
+        let username = userInfo.username;        sessionStorage.setItem('username', username);        $('#loggedInUser').text("Welcome, " + username + "!");
     }
 
     // user/register
